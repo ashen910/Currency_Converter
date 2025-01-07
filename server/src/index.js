3f98c26fdf0dda7d950f47a58538998d41835cd4
Ashen Eranda Wijenayake <89435818+ashen910@users.noreply.github.com>
2025-01-08 02:28:57 +0530

    commit

--- server/src/index.js
+++ server/src/index.js
@@ -9,6 +9,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//Get access to the All currencies
+app.get("/getAllCurrencies",async (req, res)=> {
+const nameURL ='https://docs.openexchangerates.org/reference/currencies-json?app_id=cc2f32e33aa9418eacd4b68a3f6411fd';
+
+return res.json(nameData);
+
+    try{
+        const namesResponce = await axios.get(nameURL);
+        const nameData = namesResponce.data;
+    }catch(err) {
+        console.error(err);
+    }
+
+});
+
 //listen to a port
 app.listen(3000, () =>{
     console.log("SERVER STARTED");
